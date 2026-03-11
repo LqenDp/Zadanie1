9ec6328837869c68c5f758efbd35ff51f133eeb7
LqenDp <oorzaksancaj7@gmail.com>
2026-03-11 22:52:33 +0700

    practicalwork10

--- js/main.js
+++ js/main.js
@@ -11,6 +11,81 @@ Vue.component('product-details', {
     </ul>
     `
 })
+
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </p>
+        <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+        </p>
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+        <p>
+            <label>Would you recommend this product?</label><br>
+            <input type="radio" id="recommend-yes" value="Yes" v-model="recommend">
+            <label for="recommend-yes">Yes</label>
+            <input type="radio" id="recommend-no" value="No" v-model="recommend">
+            <label for="recommend-no">No</label>
+        </p>
+        <p>
+            <input type="submit" value="Submit"> 
+        </p>
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+                this.errors = []
+            } else {
+                this.errors = []
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.recommend) this.errors.push("Recommendation required.")
+            }
+        }
+    }
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -56,7 +131,20 @@ Vue.component('product', {
                 Add to cart
             </button>
             <button v-on:click="removeFromCart">Remove from cart</button>
-        
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                        <p>Recommends: {{ review.recommend }}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted="addReview"></product-review>
         </div>
     </div>
 `,
@@ -83,7 +171,8 @@ Vue.component('product', {
                 }
             ],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-            onSale: true
+            onSale: true,
+            reviews: []
         }
     },
     methods: {
@@ -96,6 +185,9 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index;
         },
+        addReview(productReview) {
+        this.reviews.push(productReview);
+        }
 
     },
     computed: {
